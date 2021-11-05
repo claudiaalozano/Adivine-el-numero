58801e2ea62ade85c349945079fef9e2b7190e7e
claudiaalozano <clau.lozano.perez@gmail.com>
2021-11-05 18:06:21 +0100

    cambios 2

--- juego.py
+++ juego.py
@@ -1,12 +1,16 @@
 import random
 numero= random.randint(0,100)
 numero2= int(input("Por favor introduzca un número: "))
+numero_intentos= 0
 while numero != numero2:
     if numero2 < numero:
         print("Demasiado pequeño.")
         numero2= int(input("Por favor escoja otro número: "))
+        numero_intentos= numero_intentos + 1
     if numero2 > numero:
         print("Demasiado grande.")
         numero2= int(input("Por favor escoja otro número: "))
+        numero_intentos= numero_intentos + 1
 if numero2==numero:
-    print("Enohorabuena, los números coinciden.")
+    numero_intentos= numero_intentos + 1
+    print("Enohorabuena, los números coinciden." , " El número de intentos son: " , numero_intentos)
