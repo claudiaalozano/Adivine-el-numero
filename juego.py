41c83eca5b47c01b84af4966cc66610b95003584
claudiaalozano <clau.lozano.perez@gmail.com>
2021-11-05 18:00:40 +0100

    cambios

--- juego.py
+++ juego.py
@@ -1,3 +1,12 @@
 import random
 numero= random.randint(0,100)
-print("El número aleatorio es: ", numero)
+numero2= int(input("Por favor introduzca un número: "))
+while numero != numero2:
+    if numero2 < numero:
+        print("Demasiado pequeño.")
+        numero2= int(input("Por favor escoja otro número: "))
+    if numero2 > numero:
+        print("Demasiado grande.")
+        numero2= int(input("Por favor escoja otro número: "))
+if numero2==numero:
+    print("Enohorabuena, los números coinciden.")
